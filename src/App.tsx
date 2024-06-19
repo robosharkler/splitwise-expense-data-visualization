9d42b67c4ce346f01d0d80b5aee29b3ada11c20b
sharkler <mr.sharkyn@gmail.com>
2024-06-19 12:44:54 -0700

    show by date

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Papa from 'papaparse';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 import './App.css';
 
@@ -13,6 +15,9 @@ interface CSVRow {
 
 const App: React.FC = () => {
   const [expenses, setExpenses] = useState<Record<string, number>>({});
+  const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+  const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+  const [data, setData] = useState<Array<CSVRow>>([]);
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files && event.target.files[0];
@@ -25,8 +30,8 @@ const App: React.FC = () => {
             header: true,
             skipEmptyLines: true,
             complete: (results) => {
-              const data = results.data as Array<CSVRow>;
-              calculateExpenses(data);
+              const parsedData = results.data as Array<CSVRow>;
+              setData(parsedData);
             },
           });
         }
@@ -35,17 +40,23 @@ const App: React.FC = () => {
     }
   };
 
-  const calculateExpenses = (data: Array<CSVRow>) => {
+  const calculateExpenses = () => {
     const expenseMap: Record<string, number> = {};
 
     data.forEach((row) => {
-      const category = row['Category'];
-      const cost = parseFloat(row['Cost']);
-      if (!isNaN(cost)) {
-        if (expenseMap[category]) {
-          expenseMap[category] += cost;
-        } else {
-          expenseMap[category] = cost;
+      const rowDate = new Date(row['Date']);
+      if (
+        (!startDate || rowDate >= startDate) &&
+        (!endDate || rowDate <= endDate)
+      ) {
+        const category = row['Category'];
+        const cost = parseFloat(row['Cost']);
+        if (!isNaN(cost)) {
+          if (expenseMap[category]) {
+            expenseMap[category] += cost;
+          } else {
+            expenseMap[category] = cost;
+          }
         }
       }
     });
@@ -61,15 +72,38 @@ const App: React.FC = () => {
           <input type="file" accept=".csv" onChange={handleFileUpload} />
         </form>
         <div>
-          <h3>Expenses by Category:</h3>
-          <ul>
-            {Object.keys(expenses).map((category) => (
-              <li key={category}>
-                {category}: {expenses[category].toFixed(2)}
-              </li>
-            ))}
-          </ul>
+          <p>Select Date Range:</p>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date || undefined)}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            placeholderText="Start Date"
+          />
+          <DatePicker
+            selected={endDate}
+            onChange={(date) => setEndDate(date || undefined)}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            placeholderText="End Date"
+          />
         </div>
+        <button onClick={calculateExpenses}>Calculate</button>
+        {Object.keys(expenses).length > 0 && (
+          <div>
+            <h3>Expenses by Category:</h3>
+            <ul>
+              {Object.keys(expenses).map((category) => (
+                <li key={category}>
+                  {category}: {expenses[category].toFixed(2)}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </header>
     </div>
   );
